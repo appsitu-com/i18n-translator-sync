9a9eda6d99838c8e00c4c71d8d1478c4b921dfa2
Tony OHagan <301525+tohagan@users.noreply.github.com>
2026-03-10 14:45:58 +1000

    chore: restrict test coverage to src/**

--- vitest.config.ts
+++ vitest.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
       provider: 'v8',
       reporter: ['text', 'lcov'],
       reportsDirectory: './coverage',
+      include: ['src/**/*.ts'],
       exclude: ['tests/**', 'dist/**', '**/mocks/**']
     }
   },
